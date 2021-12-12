6a0d693b35414a3f03a9ba6c8c4a55c5257c8d97
GPulia <gwpuliafico@gmail.com>
2021-12-11 20:47:18 -0500

    Innital commit

---
+++ server.py
@@ -0,0 +1,25 @@
+import discord
+import requests
+
+client = discord.Client()
+
+tokenFile = open('secret.secret','r')
+token = tokenFile.readline()
+
+@client.event
+async def on_message(msg):
+    if msg.content.startswith('$$$$'):
+        name = msg.content[4::]
+        apiCall = 'https://na.whatismymmr.com/api/v1/summoner?name=' + name
+        response = requests.get(apiCall)
+        if(response.status_code == 200):
+            data = response.json()
+            Ammr = data['ARAM']['avg']
+            Apct = data['ARAM']['percentile']
+            Arank = data['ARAM']['closestRank']
+            builtMsg = f'{name}\'s ARAM MMR: {Ammr}\n{Apct}th percentile, about {Arank}'
+            await msg.channel.send(builtMsg)
+        else:
+            await msg.channel.send('RIP LMAO.')
+
+client.run(token)
